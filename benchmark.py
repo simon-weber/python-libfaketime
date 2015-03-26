9fb5a8f6ca7398a7d5550081253f43fcfbac5d54
Simon Weber <simon@simonmweber.com>
2015-03-26 16:47:29 -0400

    add benchmark

---
+++ benchmark.py
@@ -0,0 +1,36 @@
+import datetime
+import sys
+import time
+
+from libfaketime import fake_time as lft_fake_time
+from libfaketime import reexec_if_needed
+from freezegun import freeze_time as freezegun_fake_time
+
+
+def sample(faker):
+    start = time.clock()
+
+    with faker(datetime.datetime.now()):
+        datetime.datetime.now()
+
+    datetime.datetime.now()
+
+    return time.clock() - start
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'freezegun':
+        faker = freezegun_fake_time
+    else:
+        faker = lft_fake_time
+        reexec_if_needed()
+
+    iterations = 1000
+
+    print "timing %s executions of %s" % (iterations, faker)
+
+    sum = 0
+    for _ in xrange(iterations):
+        sum += sample(faker)
+
+    print sum, 'seconds'
