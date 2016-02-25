68540f484c852c8551f92b145e43513d65b892eb
André Luiz <contato@xdvl.info>
2016-02-25 10:42:27 -0300

    update supported python versions on classifiers

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     include_package_data=True,
