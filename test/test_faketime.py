63902df27f7aa1ace6f07e1c1bd54f16e03ab449
Simon Weber <simon@simonmweber.com>
2016-04-02 21:16:33 -0400

    remove re-exec tests [#11]
    I'm not really sure why these aren't working, but I have a feeling it's
    platform-dependent. I went back as far as possible and they were never
    passing on linux.

--- test/test_faketime.py
+++ test/test_faketime.py
@@ -1,7 +1,5 @@
 import datetime
-import os
 from unittest import TestCase
-import sys
 
 from mock import patch
 
@@ -10,37 +8,6 @@ from libfaketime import fake_time
 
 
 class TestReexec(TestCase):
-    def _assert_successful_reexec(self, exec_patch):
-        expected_environ = os.environ.copy()
-        expected_environ.update(libfaketime.get_reload_information()[1])
-
-        libfaketime.reexec_if_needed()
-
-        exec_patch.assert_called_once_with(
-            sys.executable, [sys.executable] + sys.argv, expected_environ
-        )
-
-    @patch('os.execve')
-    @patch('sys.platform', 'linux2')
-    def test_reexec_linux_succeeds(self, exec_patch):
-        needs_reload, env_additions = libfaketime.get_reload_information()
-
-        # We're actually running with libfaketime loaded in right now, but
-        # because we used remove_vars=True, this will return True.
-        self.assertTrue(needs_reload)
-        self.assertEqual(len(env_additions), 1)
-
-        self._assert_successful_reexec(exec_patch)
-
-    @patch('os.execve')
-    @patch('sys.platform', 'darwin')
-    def test_reexec_osx_succeeds(self, exec_patch):
-        needs_reload, env_additions = libfaketime.get_reload_information()
-        self.assertTrue(needs_reload)
-        self.assertEqual(len(env_additions), 2)
-
-        self._assert_successful_reexec(exec_patch)
-
     @patch('os.execve')
     @patch('sys.platform', 'win32')
     def test_reexec_windows_fails(self, exec_patch):
