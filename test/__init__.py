57ef1774b5bae8f976e29e14a78d3ebc1e3c0a07
Éloi Rivard <azmeuk@gmail.com>
2017-08-27 22:42:06 +0200

    Use pytest instead of nose

--- test/__init__.py
+++ test/__init__.py
@@ -1,15 +1,1 @@
-import os
-import subprocess
-import sys
 
-import libfaketime
-
-
-def setup_package():
-    ext_dir = os.path.dirname(list(libfaketime._lib_addition[sys.platform[:5]].values())[0])
-    ext_dir = os.path.join(ext_dir, '..')
-    subprocess.check_call(['make', '-C', ext_dir])
-
-    libfaketime.reexec_if_needed()
-
-    print('(re-execed and compiled prior to test run)')
