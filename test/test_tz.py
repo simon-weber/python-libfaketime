9887cd848a03f93ccc92f7c07032e76a63f25c34
Simon Weber <simon@simonmweber.com>
2018-10-27 17:10:56 -0400

    add regression test for #46

--- test/test_tz.py
+++ test/test_tz.py
@@ -1,6 +1,7 @@
 import datetime
+import os
 
-from pytest import raises
+import pytest
 from pytz import timezone
 
 from libfaketime import fake_time
@@ -34,7 +35,7 @@ def test_tzinfo_is_normalized():
 
 def test_block_setting_of_conflicting_tz_info():
     """Cannot pass in tz_offset when the timestamp already carries a timezone."""
-    with raises(Exception) as exc_info:
+    with pytest.raises(Exception) as exc_info:
         timezone_to_test_with = timezone('America/Havana')
         time_to_freeze = timezone_to_test_with.localize(datetime.datetime(2012, 10, 2, 21, 38))
 
@@ -42,3 +43,13 @@ def test_block_setting_of_conflicting_tz_info():
             pass
 
     assert str(exc_info.value) == 'Cannot set tz_offset when datetime already has timezone'
+
+
+@pytest.mark.parametrize("offset", range(-2, 3))
+def test_generated_tz_is_valid(offset):
+    """https://github.com/simon-weber/python-libfaketime/issues/46"""
+    now = datetime.datetime.now()
+
+    with fake_time(now, tz_offset=offset):
+        fake_tz = os.environ['TZ']
+        timezone(fake_tz)  # should not raise pytzdata.exceptions.TimezoneNotFound
